50784ddabbad98d5343a02a0555ebfedf8d720a9
Laurul <laly_lepadat@yahoo.ro>
2020-05-07 20:53:55 +0300

    updated game

--- Assets/Scripts/SpawnJunk.cs
+++ Assets/Scripts/SpawnJunk.cs
@@ -1,31 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnJunk : MonoBehaviour
 {
-    [SerializeField] float countDown = 1.0f;
+    [SerializeField] float countDown = 3.0f;
     [SerializeField] List<GameObject> spaceJunk;
     [SerializeField] GameObject rocketContainer;
-   
-    float timer;
+    [SerializeField] GameObject sonar;
+    Vector3 spawnPosition;
+
+
     private void Start()
     {
-        timer = countDown;
+        //spawnPosition = Random.onUnitSphere * (planetRadius + characterHeight * 0.5f) + planetPosition;
     }
 
     private void Update()
     {
         countDown -= Time.deltaTime;
-        if (countDown <= 0.0f)
+        if (countDown <= 0f)
         {
+            spawnPosition = Random.onUnitSphere * (4 + 1.05f * 0.5f) + sonar.transform.position;
+            //if (sonar.gameObject.transform.GetComponent<Sonar>().canSpawn == true)
+            //{
             int index = Random.Range(0, 3);
-            
-                Instantiate(spaceJunk[index], new Vector3(Random.Range(-4, 4), rocketContainer.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+            Instantiate(spaceJunk[index], new Vector3(spawnPosition.x, rocketContainer.transform.position.y, spawnPosition.z), Quaternion.identity);
+            //Instantiate(spaceJunk[index], new Vector3(gameObject.transform.position.x, rocketContainer.transform.position.y, this.gameObject.transform.position.z), Quaternion.identity);
 
-            countDown = timer;
+
+
+            //sonar.gameObject.transform.GetComponent<Sonar>().SwitchCanSpawn();
+
+
+            //}
+            countDown = 3.0f;
+
         }
-    
+
+
+
     }
-   
+
 }
+
+
