e292c753acaad72625ea33901c14c26af759e35c
AlexanderB99 <60917171+AlexanderB99@users.noreply.github.com>
2020-05-10 13:17:29 +0200

    1:17PM

---
+++ Assets/Scripts/StarManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarManager : MonoBehaviour
+{
+    [SerializeField] private int passScore;
+    [SerializeField] private GameObject star1;
+    [SerializeField] private GameObject star2;
+    [SerializeField] private GameObject star3;
+    [SerializeField]private bool isFinished=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        star1.gameObject.SetActive(false);
+        star2.gameObject.SetActive(false);
+        star3.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFinished==true)
+        {
+            if (passScore >= 1 && passScore <= 10)
+            { star1.gameObject.SetActive(true); }
+            if (passScore >= 11 && passScore <= 30)
+            {
+                star1.gameObject.SetActive(true);
+                star2.gameObject.SetActive(true);
+            }
+            if (passScore >= 31)
+            {
+                star1.gameObject.SetActive(true);
+                star2.gameObject.SetActive(true);
+                star3.gameObject.SetActive(true);
+            }
+        }
+    }
+}
