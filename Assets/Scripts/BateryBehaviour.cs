2bc33f31be0636006cc5810af3379682de58db3a
AlexanderB99 <60917171+AlexanderB99@users.noreply.github.com>
2020-05-18 15:38:22 +0200

    3:38PM

---
+++ Assets/Scripts/BateryBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean;
+
+public class BateryBehaviour : MonoBehaviour
+{
+    [SerializeField] Lean.Touch.LeanSelectable leanSelectable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leanSelectable.IsSelected == true)
+        {
+            this.transform.GetComponent<Rigidbody>().isKinematic = true;
+
+        }
+        else
+        {if (this.transform.GetComponent<Rigidbody>().velocity.magnitude < 0.01f)
+             this.transform.GetComponent<Rigidbody>().AddForce(-transform.up*3);
+            this.transform.GetComponent<Rigidbody>().isKinematic = false;
+            
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/BaterySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaterySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject objectToSpwan;
+    [SerializeField] float countDown = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown -= Time.deltaTime;
+        if (countDown <= 0f)
+        {
+            GameObject bateryClone =Instantiate(objectToSpwan, new Vector3(this.transform.position.x+ Random.Range(-0.7f, 0.7f), this.transform.position.y, this.transform.position.z), Quaternion.identity) as GameObject;
+            Destroy(bateryClone, 50f);
+            Rigidbody rb = bateryClone.GetComponent<Rigidbody>();
+            rb.AddForce(-transform.up*3);
+            countDown = 3.0f;
+        }
+    }
+}
