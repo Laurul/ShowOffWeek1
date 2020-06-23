3b68d92f0efe1f320152787335294c36d8790443
AlexanderB99 <60917171+AlexanderB99@users.noreply.github.com>
2020-06-23 10:43:55 +0200

    MultipleSpawn

--- Assets/Scripts/BaterySpawner.cs
+++ Assets/Scripts/BaterySpawner.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BaterySpawner : MonoBehaviour
@@ -7,7 +8,8 @@ public class BaterySpawner : MonoBehaviour
     [SerializeField] GameObject objectToSpwan;
     [SerializeField] GameObject container;
     [SerializeField] float countDown = 5.0f;
-    GameObject[] g;
+    [SerializeField] bool moreItems;
+   [SerializeField]private GameObject[] g;
     float timer;
     // Start is called before the first frame update
     void Start()
@@ -22,7 +24,10 @@ public class BaterySpawner : MonoBehaviour
       
         timer -= Time.deltaTime;
         if (timer <= 0f)
-        {
+        {if(moreItems==true)
+            {
+                MoreItems();
+            }
             GameObject bateryClone =Instantiate(objectToSpwan, new Vector3(this.transform.position.x+ Random.Range(-1f, 1f), this.transform.position.y, this.transform.position.z), this.transform.rotation) as GameObject;
            // bateryClone.transform.position = new Vector3(bateryClone.transform.position.x, bateryClone.transform.position.y, container.transform.position.z);
            
@@ -36,5 +41,13 @@ public class BaterySpawner : MonoBehaviour
         }
 
         
+    }
+    private void MoreItems()
+    {
+
+        int roll = Random.Range(0, g.Length); ;
+            objectToSpwan = g[roll].gameObject;
+        
+            
     }
 }
